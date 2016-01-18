b9f804c62406f8f46170f5cd95a7422f8ed4a341
peikun <zpkhhx@gmail.com>
2016-01-18 19:02:57 +0100

    tp02

---
+++ tp02/cell.js
@@ -0,0 +1,50 @@
+
+function Cell (v) {
+	//this.value = v || "";
+	if (v) this.value = v;
+	else this.value = "";
+
+	function getWidth () {
+		return this.height;
+	}
+
+	function getHeight () {
+		return this.getWidth;
+	}
+
+	function firstLine () {
+		if (this.cells) {
+			return this.cells[1][];
+		}
+		else 
+			return "";
+	}
+
+	function firstColumn() {
+		if (this.cells) {
+			return this.cells[1][A];
+		}
+		else 
+			return "";
+	}
+
+	function lastColumn() {
+
+	}
+
+	function getCell(c, r) {
+		
+	}
+
+
+}
+
+Cell.prototype.getValue = function  () {
+	// body...
+	return this.value;
+}
+
+Cell.prototype.setValue = function  (v) {
+	// body...
+	return this.value = v;
+}
